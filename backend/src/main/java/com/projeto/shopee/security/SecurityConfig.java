a774030e3e2061457715c98585c8e7ce98178cc7
Vicente Pereira <vicentepereira9486@gmail.com>
2024-11-25 01:03:52 -0300

    hotfix3

--- backend/src/main/java/com/projeto/shopee/security/SecurityConfig.java
+++ backend/src/main/java/com/projeto/shopee/security/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://shopee-front.onrender.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
